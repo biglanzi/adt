1ef2e58e8a261bccfa4284ec3c663119c4dd43a4
biglanzi <shenscq@foxmail.com>
2021-04-15 08:38:12 +0000

    Update protobuf2json.h
    fixed

--- demo/protobuf2json.h
+++ demo/protobuf2json.h
@@ -1,4 +1,3 @@
-
 void debug_pb2json(const ::google::protobuf::Message& msg, rapidjson::Value& jv, rapidjson::Document &doc)
 {
 	auto desc = msg.GetDescriptor();
@@ -19,21 +18,55 @@ void debug_pb2json(const ::google::protobuf::Message& msg, rapidjson::Value& jv,
 			case  ::google::protobuf::FieldDescriptor::CPPTYPE_INT32:
 				{
 					auto value = refl->GetInt32(msg, f);
-
 					jv.AddMember(rapidjson::StringRef(fname.c_str()), value, doc.GetAllocator());
 				}
 				break;
 			case ::google::protobuf::FieldDescriptor::CPPTYPE_INT64:
 				{
 					auto value = refl->GetInt64(msg, f);
-
 					jv.AddMember(rapidjson::StringRef(fname.c_str()), value, doc.GetAllocator());
+				}
+				break;
+			case ::google::protobuf::FieldDescriptor::CPPTYPE_UINT32:
+				{
+					auto value = refl->GetUInt32(msg, f);
+					jv.AddMember(rapidjson::StringRef(fname.c_str()), value, doc.GetAllocator());
+				}
+				break;
+			case ::google::protobuf::FieldDescriptor::CPPTYPE_UINT64:
+				{
+					auto value = refl->GetUInt64(msg, f);
+					jv.AddMember(rapidjson::StringRef(fname.c_str()), value, doc.GetAllocator());
+				}
+				break;
+			case ::google::protobuf::FieldDescriptor::CPPTYPE_DOUBLE:
+				{
+					auto value = refl->GetDouble(msg, f);
+					jv.AddMember(rapidjson::StringRef(fname.c_str()), value, doc.GetAllocator());
+				}
+				break;
+			case ::google::protobuf::FieldDescriptor::CPPTYPE_FLOAT:
+				{
+					auto value = refl->GetFloat(msg, f);
+					jv.AddMember(rapidjson::StringRef(fname.c_str()), value, doc.GetAllocator());
+				}
+				break;
+			case ::google::protobuf::FieldDescriptor::CPPTYPE_BOOL:
+				{
+					auto value = refl->GetBool(msg, f);
+					jv.AddMember(rapidjson::StringRef(fname.c_str()), value, doc.GetAllocator());
+				}
+				break;
+			case ::google::protobuf::FieldDescriptor::CPPTYPE_STRING:
+				{
+					auto value = std::move(refl->GetString(msg, f));
+					jv.AddMember(rapidjson::StringRef(fname.c_str()), rapidjson::Value(value.c_str(), value.length()), doc.GetAllocator());
 				}
 				break;
 			case ::google::protobuf::FieldDescriptor::CPPTYPE_MESSAGE:
 				{
 					rapidjson::Value temp(rapidjson::kObjectType);
-					debug_printf(refl->GetMessage(msg, f), temp, doc);
+					debug_pb2json(refl->GetMessage(msg, f), temp, doc);
 					jv.AddMember(rapidjson::StringRef(fname.c_str()), temp, doc.GetAllocator());
 				}
 				break;
@@ -48,9 +81,12 @@ int main()
 {
 	rapidjson::Document doc;
 	rapidjson::Value jv(rapidjson::kObjectType);
+	mLogin msg;
+	msg.set_name("john");
 	debug_pb2json(msg, jv, doc);
 	rapidjson::StringBuffer buffer;
 	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
 	jv.Accept(writer);
-	std::cout << string(buffer.GetString()) << std::endl;
+	std::cout << std::string(buffer.GetString()) << std::endl;
+	getchar();
 }
