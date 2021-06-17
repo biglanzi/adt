6e69c9df445178bfdd4523dbb2c7367e3bdaba43
biglanzi <shenscq@foxmail.com>
2021-06-17 02:59:45 +0000

    Create pushWx.sh
    you can push message to wechat with one test accout provided by tecent

---
+++ shell/pushWx.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -exo
+TokenFile="token.data"
+to_user=("openidxx" "openidxx")
+
+function PushWarning()
+{
+# $1:tousr	
+# $2:msg	 
+# $3:token
+    curl -X POST -d "{\"touser\":\"${1}\",\"msgtype\":\"text\",\"text\":{\"content\":\"${2}\"}}" "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=${3}"
+}
+function GetToken()
+{
+	now=$(date +%s)
+	if [ -f $TokenFile ] && [ $((now-$(date +%s -r $TokenFile))) -lt 1800 ];then
+		cat $TokenFile
+	else
+    		local appid="${appid}"
+	    	local secret="${secret}"
+    		local token=$(curl "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}"|jq ".access_token"|sed 's#\"##g')
+		echo $token > $TokenFile
+		echo $token
+	fi
+}
+
+token=$(GetToken)
+echo "token=${token}"
+msg="aaddcc"
+for user in ${to_user[@]}
+do
+PushWarning ${user} "${msg}" ${token}
+done
